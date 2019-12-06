9586ecec7f001975c339910de195e1ef0bd61cf0
Dale Smith <dtsmith@vallumsoftware.com>
2019-12-06 14:22:53 -0500

    several bazel options broken out

--- Dockerfiles/install-ubuntu.sh
+++ Dockerfiles/install-ubuntu.sh
@@ -41,13 +41,16 @@ apt-get -y install \
 
 if $shared; then
     # install bazel for the shared library version
-    export BAZEL_VERSION=${BAZEL_VERSION:-`cat ./tensorflow_cc/Dockerfiles/BAZEL_VERSION`}
-    apt-get -y install pkg-config zip g++ zlib1g-dev unzip python
-    bazel_installer=bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
-    wget -P /tmp https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${bazel_installer}
-    chmod +x /tmp/${bazel_installer}
-    /tmp/${bazel_installer}
-    rm /tmp/${bazel_installer}
+    command -v bazel > /dev/null 2>&1 || (
+        export BAZEL_VERSION=${BAZEL_VERSION:-`cat ./tensorflow_cc/Dockerfiles/BAZEL_VERSION`}
+        echo "Installing bazel version ${BAZEL_VERSION}"
+        apt-get -y install pkg-config zip g++ zlib1g-dev unzip python
+        bazel_installer=bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+        wget -P /tmp https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${bazel_installer}
+        chmod +x /tmp/${bazel_installer}
+        /tmp/${bazel_installer}
+        rm /tmp/${bazel_installer}
+    )
 fi
 if $cuda; then
     # install libcupti
